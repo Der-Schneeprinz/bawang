1ce5ca16971d1d96ea188dc70329cd7a878c10c9
Der Kreidekönig <mcolaguer@iskolarngbayan.pup.edu.ph>
2023-01-26 04:26:42 +0800

    Migrated into MVVM Architecture

---
+++ app/src/main/java/com/merah/bawang/view/homescreen/fragments/DiscoverFragment.java
@@ -0,0 +1,69 @@
+package com.merah.bawang.view.homescreen.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.merah.bawang.R;
+import com.merah.bawang.model.Post;
+import com.merah.bawang.viewmodel.recyclerviewposts.RecyclerViewAdapter;
+
+import java.util.ArrayList;
+
+public class DiscoverFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    ArrayList<Post> itemPostContent = new ArrayList<>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discover, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //setUpItemPostContent();
+
+        //recyclerView = view.findViewById(R.id.recyclerView);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        //recyclerView.setHasFixedSize(true);
+        //RecyclerViewAdapter adapter = new RecyclerViewAdapter(getActivity(), itemPostContent);
+        //recyclerView.setAdapter(adapter);
+        //adapter.notifyDataSetChanged();
+    }
+
+
+    private void setUpItemPostContent() {
+        // conUsername
+        String[] postUsername = getResources().getStringArray(R.array.saUsernames);
+        String[] postOrg = getResources().getStringArray(R.array.saOrgs);
+        for(int i = 0; i < postUsername.length;i++){
+            itemPostContent.add(new Post(
+                    postUsername[i],
+                    postOrg[i%2],
+                    getResources().getString(R.string.postText),
+                    getResources().getString(R.string.postTitle),
+                    0,
+                    0,
+                    R.drawable.ic_baseline_account_circle_24
+            ));
+        }
+    }
+}
